3cae284e62a9abf111dee63b7a6f665082966ca5
Piyush Sachan <piyush.fgk@gmail.com>
2021-05-21 17:44:05 +0530

    ToDo App with with Material UI

---
+++ todo/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainUI from './components/MainUI'
+
+
+const App = () => {
+    return (
+        <>
+            <MainUI />
+        </>
+    )
+}
+
+export default App
